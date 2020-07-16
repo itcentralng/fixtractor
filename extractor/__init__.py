93a03fed2633267f530765777d9f76c98c41d299
Nasir Mustapha <teboy.com@gmail.com>
2020-07-16 03:11:01 +0100

    fixed bug, added python-magic dependency

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import argparse, os, mimetypes
+import argparse, os, magic
 
 from shutil import copyfile
 
@@ -12,9 +12,15 @@ parser.add_argument("-e", "--extract", help="Full Directory Path e.g. '/Users/so
 args = parser.parse_args()
 
 def extract():
+    print('Intializing restore.....')
     error_count = 0
     extracted = "restored"
-    os.makedirs(extracted)
+    print('creating restore folder.....')
+    try:
+        os.makedirs(extracted)
+    except:
+        print(f'{extracted} folder found')
+        pass
     backup_folder = args.extract
     '''
     itunes backup folder looks like this:
@@ -28,32 +34,37 @@ def extract():
                     file, file, file
     '''
     if backup_folder:
+        print('Checking backup folder.....')
         for folder in os.listdir(backup_folder):
             '''
             get a list of everything in the backup_folder
             and identify directories
             '''
             path = f"{backup_folder}/{folder}"
+            print(f"Found {path}")
             if os.path.isdir(path):
+                print(f"Looking for files from {path}")
                 '''
                 go through all files in the folder
                 '''
-                for _file in path:
+                for _file in os.listdir(path):
                     '''
                     ignore if hidden files
                     '''
-                    if not _file.startswith('.'):
-                        file_path = f"{path}/{_file}"
-                        _type = mimetypes.guess_type(file_path)
+                    file_path = f"{path}/{_file}"
+                    if not _file.startswith('.') and not os.path.isdir(file_path):
+                        print(f"Found {_file}")
+                        _type = magic.from_file(file_path, mime=True)
                         # _types: (image/png, None)
-                        extension = _type[0].split('/')[1]
+                        extension = _type.split('/')[1]
                         corrected_file = f"{_file}.{extension}"
                         # corrected_file: file.ext
                         # copyfile(src, dst)
                         new_file = f"{extracted}/{corrected_file}"
                         # new_file: restored/file.ext
+                        print(f"{_file} has been restored to {corrected_file}")
                         try:
-                            copyfile(corrected_file, new_file)
+                            copyfile(file_path, new_file)
                         except Exception as e:
                             print(e)
                             error_count+=1
